e580804ad89c502f6fbec814d166339af0d88332
sgraff <copellius@gmail.com>
2013-10-20 16:26:25 -0700

    Initial and Bad Layout

--- Total-Recall/gen/edu/vt/cs5744/android/totalrecall/R.java
+++ Total-Recall/gen/edu/vt/cs5744/android/totalrecall/R.java
@@ -35,7 +35,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int boats_button=0x7f050004;
+        public static final int consumer_products_button=0x7f050002;
+        public static final int cosmetics_button=0x7f050007;
+        public static final int environmental_button=0x7f050008;
+        public static final int food_button=0x7f050005;
+        public static final int medicine_button=0x7f050006;
+        public static final int motor_vehicles_button=0x7f050003;
     }
     public static final class style {
         /** 
